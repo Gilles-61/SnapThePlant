dc683d53cecbd731f33c550bca478c1c45c4fe8a
Gilles Nobert <GillesNobert@gmail.com>
2025-08-05 23:18:07 +0000

    just hide this too that will fix unautherized users

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,11 @@
+
 'use client';
 
 import { LoginForm } from '@/components/login-form';
-import { AuthHeader } from '@/components/auth-header';
 
 export default function LoginPage() {
   return (
     <div className="flex flex-col min-h-screen bg-background text-foreground">
-      <AuthHeader />
       <main className="flex-1 bg-background flex items-center justify-center">
         <LoginForm />
       </main>
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,12 +1,11 @@
+
 'use client';
 
 import { SignUpForm } from '@/components/signup-form';
-import { AuthHeader } from '@/components/auth-header';
 
 export default function SignUpPage() {
   return (
     <div className="flex flex-col min-h-screen bg-background text-foreground">
-      <AuthHeader />
       <main className="flex-1 bg-background flex items-center justify-center">
         <SignUpForm />
       </main>
