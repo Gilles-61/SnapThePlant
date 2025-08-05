de676c230ff883709d5d536c2483decc49d2b890
Gilles Nobert <GillesNobert@gmail.com>
2025-08-05 20:44:59 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,10 +1,11 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp, type FirebaseOptions } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 export const firebaseConfig: FirebaseOptions = {
   "projectId": "snaptheplant-2qivc",
-  "appId": "1:766003553871:web:8a7a006940d561c8806c3a",
+  "appId": "1:766003553871:web:53fda5c8df2903ba806c3a",
   "storageBucket": "snaptheplant-2qivc.firebasestorage.app",
   "apiKey": "AIzaSyDkplRwx8wkVb5J73j48zt85LlZvzXaIkw",
   "authDomain": "snaptheplant-2qivc.firebaseapp.com",
